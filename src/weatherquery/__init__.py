3eb5a325d837302cf467d1fc806272f8b26cfc99
fexcode <14356165+fexcode@user.noreply.gitee.com>
2025-07-15 14:08:24 +0800

    test(新增关于Qurier的测试) fix(改善用户传入不存在城市时的报错信息) test(新增“测试全部”脚本) fix(修复Querier查询today的时候的类型错误)

--- src/weatherquery/__init__.py
+++ src/weatherquery/__init__.py
@@ -1,13 +1,18 @@
 from .getapi import GetApi
 from .core import *
+from .consts import LOGGER
 
 
 def debug():
-    from .consts import LOGGER
-
     # LOGGER.debug(get_api_by_citycode("101020100"))
     # LOGGER.debug(get_citycode_from_cityname("芜湖市"))
     # LOGGER.debug(GetApi.get_city_weather_dict("芜湖"))
-    q = Querier("芜湖")
+    q = Querier("北京市")
+    LOGGER.debug(q.forecast)
     LOGGER.debug(q.cityInfo)
-    LOGGER.debug(q.mainData)
+    # LOGGER.debug(q.mainData)
+
+    q = Querier("呼和浩特")
+    LOGGER.debug(q.today)
+    LOGGER.debug(q.cityInfo)
+    # LOGGER.debug(q.mainData)
